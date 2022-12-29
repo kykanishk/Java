f949e1087a1e134d0f172eb9b6a97a5d0ec23a4f
kanishk <kykanishk@gmail.com>
2022-12-30 00:49:50 +0530

    half pyramid pattern java

---
+++ Pattern.java
@@ -0,0 +1,11 @@
+public class Pattern {
+    public static void main(String[] args) {
+       int n=7;
+       for(int i=0; i<n; i++){
+           for(int j=1; j<=n; j++){
+               if(i+j<n) System.out.print(" ");
+               else System.out.print("*");
+       }
+           System.out.println();
+    }
+}}
